536b3b3295fc8f8eeabb0f10d3d74d8eb80ff77b
Daniel Borges Leal <danielleal94@gmail.com>
2015-10-27 21:06:19 -0300

    get image from camera

---
+++ handler.py
@@ -0,0 +1,15 @@
+'''
+@Autor:         Leal
+@Created_on:    27/10/15
+'''
+
+class Handler(object):
+
+    def low_passing(self, img):
+        pass
+
+    def high_passing(self, img):
+        pass
+
+    def sobel(self, img):
+        pass
--- main.py
+++ main.py
@@ -3,4 +3,40 @@
 @Created_on:    27/10/15
 '''
 
-import SimpleCV
+from SimpleCV import *
+import handler as hnd
+
+if __name__ == '__main__':
+
+    # init camera
+    cam = Camera(prop_set={"width":600,"height":400})
+
+    # init display
+    disp = Display()
+
+    # Show imagem while user didn't click
+    while disp.isNotDone():
+            # get webcam image
+            img = cam.getImage()
+            # save image and close when left click
+            if disp.mouseLeft:
+                    img.save('selfie.png')
+                    break
+            img.save(disp)
+
+
+    # Load Image
+    selfie = Image('selfie.png')
+
+    # create handler object
+    filter_hnd = hnd.Handler()
+
+    # Apply Filters
+    lpf = filter_hnd.low_passing(selfie)
+    #lpf.save('low_passing_selfie.png')
+
+    hpf = filter_hnd.low_passing(selfie)
+    #hpf.save('high_passing_selfie.png')
+
+    sobel = filter_hnd.low_passing(selfie)
+    #sobel.save('sobel_selfie.png')
